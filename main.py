51b37992a3fbd16b19756139803c60af42f4bf1e
Luissoutelo <44030449+Luissoutelo@users.noreply.github.com>
2026-07-25 15:07:55 +0100

    Update PDF path in question function
    Replaced the hardcoded PDF path with a placeholder.

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def home():
 
 @app.get("/question")
 def question(question: str):
-    answer = answer_question(question, r"C:\Dev\python-fundamentos\exercicos\Zoho_Caderno_Encargos_CRM_ServicosEnergias.pdf")  # r tells Python to ignore \ as escape characters
+    answer = answer_question(question,"PDF path")
     return answer
